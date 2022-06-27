950e07e5325a368fa16c5f58ccf1fe3fd4a096b4
rakesh.k <rakesh.k@gofrugal.com>
2022-06-27 16:14:59 +0530

    Rakesh|fix| added mobile validation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'furniture-magik-clone';
+  isMobile: boolean = false;
+
+  ngOnInit(): void {
+    let details = navigator.userAgent;
+    let regexp = /android|iphone|kindle|ipad/i;
+    this.isMobile = regexp.test(details);
+  }
+
 }
